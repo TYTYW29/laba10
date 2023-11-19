0743e880d82b708d2e287d316c0c8b6c4c750b20
TYTYW29 <a.hayevskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-19 18:36:34 +0200

    5-1.2

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@ int main() {
     int v1;
     int p1;
     double answ;
-    std::cout << "Введіть об'єм бака: " << std::endl;
+    std::cout << "Вартість одного номера: " << std::endl;
     std::cin >> v1 ;
 
-    std::cout << "Введіть ціну палива (грн/л): " << std::endl;
+    std::cout << "Періодичність видання: " << std::endl;
     std::cin >> p1 ;
-    answ = (p1*v1) ;
-    std::cout << "Вартість пального в баку " <<answ<< std::endl;
+    answ = (p1*v1*12) ;
+    std::cout << "Вартість річної підписки " <<answ<< std::endl;
     return 0;
 }
