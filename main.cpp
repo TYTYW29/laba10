8fd1259550f9ddf7e51720b71181522752f73173
TYTYW29 <a.hayevskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-20 18:27:43 +0200

    5-3

--- main.cpp
+++ main.cpp
@@ -3,30 +3,49 @@
 
 int main() {
     system("chcp 65001");
-    double Ax = 0, Ay = 0;
-    double Bx = 5, By = 5 - 1;
-    double Cx = -5, Cy = 5 + 1;
+
+//    std::cout << "Введіть значення кута a в градусах: ";
+    double a;
+//    std::cin >> a ;
+    double z1;
+    double z2;
+    double answ_1;
+    double answ;
+    double s = 0;
+    double sin= s;
+    double c;
+    double cos = c;
 
 
-    double median_Bx = (Ax + Bx) / 2;
-    double median_By = (Ay + By) / 2;
-
-
-    double median_length_B = sqrt(pow(Bx - median_Bx, 2) + pow(By - median_By, 2));
-
-
-    double mid_ACx = (Ax + Cx) / 2;
-    double mid_ACy = (Ay + Cy) / 2;
-
-
-    double length_AC = sqrt(pow(Cx - Ax, 2) + pow(Cy - Ay, 2));
-
-    
-    double bisector_length_C = (2 * length_AC * sqrt(1 + pow(sqrt(pow(Bx - mid_ACx, 2) + pow(By - mid_ACy, 2)) / length_AC, 2))) / (sqrt(pow(Bx - mid_ACx, 2) + pow(By - mid_ACy, 2)) + length_AC);
-
-    std::cout << "Довжина медіани  B: " << median_length_B << std::endl;
-    std::cout << "Довжина бісектриси  C: " << bisector_length_C << std::endl;
-
+//    double result1 = 1 - 1/4 * std::pow(std::sin(2 * a), 2) + 3 * std::cos(a);
+//    double result2 = std::pow(std::sin(a), 2) + std::pow(std::sin(4 * a), 2);
+    answ = 1 - 1/4 * pow(s , 2) * 2 * a + 3 * c * a;
+    answ_1 = pow(c , 2) * a + pow(c , 4) * a;
+    std::cout << "Значення виразу z1: " << answ << std::endl;
+    std::cout << "Значення виразу z2: " << answ_1 << std::endl;
     return 0;
 }
+// #include <iostream>
+// #include <cmath>
+// #include <math.h>
+//
+//int main() {
+//
+//    double test_cases [] = {0,  M_PI/2,  M_PI / 4, M_PI};
+//    int num_test_cases = sizeof(test_cases) / sizeof(test_cases[0]);
+//
+//    for (int i = 0; i < num_test_cases; ++i) {
+//        double alpha = test_cases[i];
+//
+//        double result1 = 1 - 1/4 * std::pow(std::sin(2 * alpha), 2) + 3 * std::cos(alpha);
+//        double result2 = std::pow(std::sin(alpha), 2) + std::pow(std::sin(4 * alpha), 2);
+//
+//        std::cout << "alpha = " << alpha << std::endl;
+//        std::cout << "Formula 1 result: " << result1 << std::endl;
+//        std::cout << "Formula 2 result: " << result2 << std::endl;
+//        std::cout << "---" << std::endl;
+//    }
+//
+//    return 0;
+//}
 
