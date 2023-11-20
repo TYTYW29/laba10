7e38cbe83e15aed5b05a5bb4cd24135c85251aeb
TYTYW29 <a.hayevskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-20 16:41:02 +0200

    5-2

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
     system("chcp 65001");
-    int v1;
-    int p1;
-    double answ;
-    std::cout << "Вартість одного номера: " << std::endl;
-    std::cin >> v1 ;
+    double Ax = 0, Ay = 0;
+    double Bx = 5, By = 5 - 1;
+    double Cx = -5, Cy = 5 + 1;
 
-    std::cout << "Періодичність видання: " << std::endl;
-    std::cin >> p1 ;
-    answ = (p1*v1*12) ;
-    std::cout << "Вартість річної підписки " <<answ<< std::endl;
+
+    double median_Bx = (Ax + Bx) / 2;
+    double median_By = (Ay + By) / 2;
+
+
+    double median_length_B = sqrt(pow(Bx - median_Bx, 2) + pow(By - median_By, 2));
+
+
+    double mid_ACx = (Ax + Cx) / 2;
+    double mid_ACy = (Ay + Cy) / 2;
+
+
+    double length_AC = sqrt(pow(Cx - Ax, 2) + pow(Cy - Ay, 2));
+
+    
+    double bisector_length_C = (2 * length_AC * sqrt(1 + pow(sqrt(pow(Bx - mid_ACx, 2) + pow(By - mid_ACy, 2)) / length_AC, 2))) / (sqrt(pow(Bx - mid_ACx, 2) + pow(By - mid_ACy, 2)) + length_AC);
+
+    std::cout << "Довжина медіани  B: " << median_length_B << std::endl;
+    std::cout << "Довжина бісектриси  C: " << bisector_length_C << std::endl;
+
     return 0;
 }
+
