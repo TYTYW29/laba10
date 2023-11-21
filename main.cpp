e4bca2297732c2bf3c6926431e0764d935839d44
TYTYW29 <a.hayevskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-21 23:34:27 +0200

    5-5

--- main.cpp
+++ main.cpp
@@ -4,16 +4,26 @@
 int main() {
     system("chcp 65001");
 
-    int y;
-    const int x = 2.1;
-    const int p = 2;
-    int a;
-    int b;
-    y = std::pow(a, 3) / pow(b, 2);
-    a = exp(sqrt(fabs(x)));
-    b = ((sin(pow(p, 2))) + pow(x, 3));
-    std::cout << y << std::endl;
-    return 0;
+    double b, a, s, answer;
+    std::cout << "введіть значення b:";
+    std::cin >> b ;
+    std::cout << "введіть значення a:";
+    std::cin >> a;
+    std::cout << "введіть значення s:";
+    std::cin >> s;
+
+
+
+    answer = ((34 * sin(b) + a) / (sqrt(2 * a + 4 * s / (5 * s)))) + 4 * a * b;
+
+    if (a != -0.4) {
+        std::cout << answer;
+    }
+    else {
+        std::cout << "a не знаходиться в області визначення " << std::endl;
+    }
+
+
 }
 
 
